086231ca4faa65e75376341bb3f0fe01a0a3b1bc
= <=>
2021-12-01 14:52:53 +0100

    fix font

--- login.php
+++ login.php
@@ -1,54 +1,47 @@
 <?php
 session_start();
-    include("connection.php");
-    include("functions.php");
+include("connection.php");
+include("functions.php");
 
-    if($_SERVER['REQUEST_METHOD'] == "POST")
-    {
-        // something was posted
-        $user_name = $_POST['username'];
-        $password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    // something was posted
+    $user_name = $_POST['username'];
+    $password = $_POST['password'];
 
-        if(!empty($user_name)&& !empty($password) && !is_numeric($user_name))
-        {
-            //read from database & find user with the matching username
-            $query = "select * from user where username = '$user_name' limit 1";
+    if (!empty($user_name) && !empty($password) && !is_numeric($user_name)) {
+        //read from database & find user with the matching username
+        $query = "select * from user where username = '$user_name' limit 1";
 
-            $result = mysqli_query($con, $query);
+        $result = mysqli_query($con, $query);
 
-            //if user was found check if password is correct
-            if($result)
-            {
-                if($result && mysqli_num_rows($result) > 0)
-                {
-                    $user_data = mysqli_fetch_assoc($result);
+        //if user was found check if password is correct
+        if ($result) {
+            if ($result && mysqli_num_rows($result) > 0) {
+                $user_data = mysqli_fetch_assoc($result);
 
-                    if($user_data['password'] === $password)
-                    {
-                      $_SESSION['username'] = $user_data['username'];
-                      header("Location: index.php");
-                      die;
-                    }
+                if ($user_data['password'] === $password) {
+                    $_SESSION['username'] = $user_data['username'];
+                    header("Location: index.php");
+                    die;
                 }
             }
         }
-
-        else
-        {
-           echo "please enter some valid information";
-        }
+    } else {
+        echo "please enter some valid information";
     }
+}
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" href="assets/css/style.css">
-<script src="assets/css/js/scripts.js"></script>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/css/js/script.js"></script>
 
     <title>Login</title>
 
@@ -58,19 +51,20 @@ session_start();
 
 <body>
 
-   <div id ="cent">
-       <h3>Login</h3><br>
-       <form method = "POST">
+    <div id="cent">
+        <h3>Login</h3><br>
+        <form method="POST">
 
-           <p class="center">Username</p><br><input type ="text" name="username" ><br><br>
-           <p>Password</p><br><input type ="password" name="password"><br><br><br>
+            <p class="center">Username</p><br><input type="text" name="username"><br><br>
+            <p>Password</p><br><input type="password" name="password"><br><br><br>
 
 
-           <input type="submit" class="button button1" value = "Login"><br><br><br>
-           <a href ="register.php" class="button button1" style="text-decoration: none">Register</a><br><br><br>
-       </form>
-   </div>
+            <input type="submit" class="button button1" value="Login"><br><br><br>
+            <a href="register.php" class="button button1" style="text-decoration: none">Register</a><br><br><br>
+        </form>
+    </div>
 
 
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -8,32 +8,28 @@ require_once('includes/classes/Account.php');
 require_once('assets/css/style.css');
 */
 session_start();
-    include("connection.php");
-    include("functions.php");
+include("connection.php");
+include("functions.php");
 
-    if($_SERVER['REQUEST_METHOD'] == "POST")
-    {
-        // something was posted
-        //
-        $user_name = $_POST['username'];
-        $password = $_POST['password'];
-        $mail_address = $_POST['mail_address'];
-        $age = $_POST['age'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    // something was posted
+    //
+    $user_name = $_POST['username'];
+    $password = $_POST['password'];
+    $mail_address = $_POST['mail_address'];
+    $age = $_POST['age'];
 
-        if(!empty($user_name)&& !empty($password) && !is_numeric($user_name))
-        {
-            //save to database
-            $query = "insert into user (username,password,mail_address,age) values('$user_name','$password','$mail_mail_address','$age')";
+    if (!empty($user_name) && !empty($password) && !is_numeric($user_name)) {
+        //save to database
+        $query = "insert into user (username,password,mail_address,age) values('$user_name','$password','$mail_mail_address','$age')";
 
-            mysqli_query($con, $query);
-            header("Location: login.php");
-            die;
-        }
-        else
-        {
-            echo "Please enter some valid information!";
-        }
+        mysqli_query($con, $query);
+        header("Location: login.php");
+        die;
+    } else {
+        echo "Please enter some valid information!";
     }
+}
 ?>
 
 
@@ -47,25 +43,26 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="assets/css/style.css">
-    <script src="assets/css/js/scripts.js"></script>
+    <script src="assets/css/js/script.js"></script>
 
     <title>Register</title>
 </head>
 
 <body>
-   <div id = "cent">
-           <h3>Register</h3><br>
+    <div id="cent">
+        <h3>Register</h3><br>
 
-        <form method = "POST">
+        <form method="POST">
 
-           <p>Username</p><input type ="text" name="username" required><br><br>
-           <p>Password</p><input type ="password" name="password" required><br><br>
-           <p>E-Mail</p><input type = "email" name ="mail_address" required><br><br>
-           <p>Age</p><input type = "number" name ="age" min = "18", max = "100" required><br><br>
+            <p>Username</p><input type="text" name="username" required><br><br>
+            <p>Password</p><input type="password" name="password" required><br><br>
+            <p>E-Mail</p><input type="email" name="mail_address" required><br><br>
+            <p>Age</p><input type="number" name="age" min="18" , max="100" required><br><br>
 
-           <input type="submit" class="button button1" value = "Register"><br><br>
-           <a href ="login.php" class="button button1"style="text-decoration: none">Login</a></button><br><br>
+            <input type="submit" class="button button1" value="Register"><br><br>
+            <a href="login.php" class="button button1" style="text-decoration: none">Login</a></button><br><br>
         </form>
-   </div>
+    </div>
 </body>
+
 </html>
