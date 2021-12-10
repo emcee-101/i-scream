17da4da6870c73353ccc400739a642141a32f4a7
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2021-12-10 18:12:55 +0100

    added functions to retrieve Data about Movies from DB

--- functions.php
+++ functions.php
@@ -21,3 +21,60 @@ function check_login($con)
     header("Location: login.php");
     die;
 }
+
+
+function getBanner(){
+
+        //include("connection.php");
+
+
+            //get a banner with id
+        $query = "SELECT `id`, `image_path` FROM `banner_images` LIMIT 3;";
+        $parsed_query = mysqli_query($con, $query);
+        $results = array();
+
+            //put lines of values in one array AS one array and repeat
+        while ($line = mysqli_fetch_array($parsed_query, MYSQLI_ASSOC)) {
+            $results[] = $line;
+        }
+
+                /* associative array - how the data is returned:*/
+//        echo $results[0]["id"].$results[0]["image_path"]."\n";
+//        echo $results[1]["id"].$results[1]["image_path"];
+
+        return $results;
+
+    }
+
+
+function getGroup(WantMovie){
+
+        //include("connection.php");
+        // to test it get Group with id = 1
+        $VidGrID = 1;
+
+            //get a Title of a Group of titles by id of group (AKA video_group_id)
+        $query = "SELECT title FROM video_group WHERE video_group_id = ".$VidGrID." and isMovies = ".$WantMovie." LIMIT 1;";
+
+        $parsed_query = mysqli_query($con, $query);
+
+        $title =  mysqli_fetch_assoc($result);
+
+
+        $query = "SELECT entity_id, title FROM video_group_member WHERE video_group_id = ".$VidGrID.";";
+        $parsed_query = mysqli_query($con, $query);
+        $results = array();
+
+            //put lines of values in one array AS one array and repeat   array(array(value1,value2),array(value1,value2)) .....
+        while ($line = mysqli_fetch_array($parsed_query, MYSQLI_ASSOC)) {
+            $results[] = $line;
+        }
+
+                /* associative array - how the data is returned:*/
+//        echo $results[0]["entity_id"].$results[0]["title"]."\n";
+//        echo $results[1]["entity_id"].$results[1]["title"];
+
+
+        return $title, $results;
+
+    }
