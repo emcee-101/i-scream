3c1ad20898b615c34db38f430126481e36eeb665
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2021-12-13 18:05:50 +0100

    made the video boxes object oriented, so the process of creating can be automatised

--- movies.php
+++ movies.php
@@ -23,36 +23,80 @@ session_start();
 
 <body class="background3">
   <div class="fade-in">
-    <h3 id="whitefont" style="margin:5px;" >Zombie</h3> 
 
-    <div class="videowrapper">
-      <div ><img class= "videobox"src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-      <div ><img class= "videobox" src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div> 
-      <div ><img class= "videobox" src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-      <div ><img class= "videobox"src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-      <div ><img class= "videobox" src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-    </div>
 
-    <h3 id="whitefont" style="margin:5px;">Haunted</h3>
 
-    <div class="videowrapper">
-      <div ><img class= "videobox" src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-      <div ><img class= "videobox"src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-      <div ><img class= "videobox" src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div> 
-      <div ><img class= "videobox" src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-      <div ><img class= "videobox"src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-    </div>
+  <?php
+        // Class is Category of Movie or TV Show with title and basic styling and a property of an array of "vidBoxObjects"
+        class vidBoxWrapper {
+            private $title ="";
+            private $grID =0;
+            private $vidGroupElements = array();
 
-    <h3 id="whitefont" style="margin:5px;">Psycho</h3>
+            private function getVidBoxElements(){
 
-    <div class="videowrapper">
-      <div ><img class= "videobox" src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div> 
-      <div ><img class= "videobox" src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-      <div ><img class= "videobox"src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-      <div ><img class= "videobox" src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div> 
-      <div ><img class= "videobox" src="https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg"></div>
-    </div>
-  </div> 
+                //print out the Output of getString() of each of the "vidGroupMem" elements in the Array
+
+                $tmp = "";
+
+               foreach($this->vidGroupElements as $element){
+
+                    $tmp .= $element->getString();
+               }
+                return $tmp;
+            }
+
+            public function getString() {
+
+                //print out wrapper
+
+                $str = "<h3 id='whitefont' style='margin:5px;'>".$this->title."</h3>";
+                $str .= "<div class='videowrapper'>";
+                $str .= $this->getVidBoxElements();
+                $str .= "</div>";
+                return $str;
+            }
+
+            public function __construct($title, $grID, $vidGroupElements){
+                $this->title = $title;
+                $this->grID = $grID;
+                $this->vidGroupElements = $vidGroupElements;
+            }
+
+    }
+
+
+        class vidBoxElement {
+        private $img ="";
+        private $ent_ID = 0;
+        public function getString() {
+
+                    // print out image tag with correct source and basic styling
+
+            $str = "<div >";
+            $str .= "<img class= 'videobox' src='".$this->img."'>";
+            $str .= "</div>";
+            return $str;
+        }
+        public function __construct($imgurl, $ent_id){
+            $this->img = $imgurl;
+            $this->ent_id = $ent_id;
+        }
+
+    }
+
+    //testdata
+
+    $test = array(
+        new vidBoxElement("https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg",1),
+        new vidBoxElement("https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg",1),
+        new vidBoxElement("https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg",1)
+            );
+    $test2 = new vidBoxWrapper("Halloween Special", 1, $test);
+    echo $test2->getString();
+  ?>
+
+
 
 </body>
 </html>
