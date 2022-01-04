41974f50baa0fa77d31141649e7fc75dc42ba84b
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-01-04 17:51:03 +0100

    Moved Classes for Visual Elements in movies.php to tincludes/classes/DisplayElements.php

---
+++ includes/classes/DisplayElements.php
@@ -0,0 +1,97 @@
+ 
+<?php
+
+   // Class is Category of Movie or TV Show with title and basic styling and a property of an array of "vidBoxObjects"
+        class vidBoxWrapper {
+            private $title ="";
+            private $grID =0;
+            private $vidGroupElements = array();
+
+            private function getVidBoxElements(){
+
+                //print out the Output of getString() of each of the "vidGroupMem" elements in the Array
+
+                $tmp = "";
+
+               foreach($this->vidGroupElements as $element){
+
+                    $tmp .= $element->getString();
+               }
+                return $tmp;
+            }
+
+            public function getString() {
+
+                //print out wrapper
+
+
+                $str = "<h3 id='whitefont' style='margin:5px;'>".$this->title."</h3>";
+                $str .= "<div class='videowrapper'>";
+                $str .= $this->getVidBoxElements();
+                $str .= "</div>";
+
+                return $str;
+            }
+
+            public function __construct($title, $grID, $vidGroupElements){
+                $this->title = $title;
+                $this->grID = $grID;
+                $this->vidGroupElements = $vidGroupElements;
+            }
+
+    }
+
+
+        class vidBoxElement {
+        private $img ="";
+        private $ent_ID;
+        private $isInWatchlist;
+
+        public function getWatchListModule() {
+
+            // submit action to watchlist.php per php "get" when clicked
+            $str = "<a href='watchlist.php?action=status&ent_id=".$this->ent_ID."'>";
+
+            // if already watchlisted: ask to remove
+            if ($this->isInWatchlist == 1){
+                    $str .= "<h4 class='addwatchlist'>- Remove from Watchlist</h4>";
+            }
+            else{
+                    $str .= "<h4 class='addwatchlist'>+ Add to Watchlist</h4>";
+            }
+
+
+            $str .= "</a>";
+            return $str;
+
+        }
+
+        public function getString() {
+
+                    // print out image tag with correct source and basic styling
+
+            $str = "<div>";
+            $str .= "<a href=".getWatchURL($this->ent_ID).">";
+            $str .= "<img class= 'videobox' src=".$this->img.">";
+            $str .= "</a>";
+            $str .= $this->getWatchListModule();
+            $str .= "</div>";
+            return $str;
+        }
+        public function __construct($imgurl, $ent_id){
+
+            $this->img = $imgurl;
+            $this->ent_ID = $ent_id;
+
+            $this->isInWatchlist = checkIfWatchlisted($_SESSION['usr_id'],$this->ent_ID);
+
+
+
+
+        }
+
+    }
+
+
+
+?>
--- movies.php
+++ movies.php
@@ -3,6 +3,7 @@ require_once("includes/header.php");
 require_once("includes/footer.php");
 include("includes/connection.php");
 include("includes/functions.php");
+include("includes/classes/DisplayElements.php");
 
 session_start();
 
@@ -28,110 +29,9 @@ session_start();
 
 
   <?php
-        // Class is Category of Movie or TV Show with title and basic styling and a property of an array of "vidBoxObjects"
-        class vidBoxWrapper {
-            private $title ="";
-            private $grID =0;
-            private $vidGroupElements = array();
-
-            private function getVidBoxElements(){
-
-                //print out the Output of getString() of each of the "vidGroupMem" elements in the Array
-
-                $tmp = "";
-
-               foreach($this->vidGroupElements as $element){
-
-                    $tmp .= $element->getString();
-               }
-                return $tmp;
-            }
-
-            public function getString() {
-
-                //print out wrapper
 
 
-                $str = "<h3 id='whitefont' style='margin:5px;'>".$this->title."</h3>";
-                $str .= "<div class='videowrapper'>";
-                $str .= $this->getVidBoxElements();
-                $str .= "</div>";
-
-                return $str;
-            }
-
-            public function __construct($title, $grID, $vidGroupElements){
-                $this->title = $title;
-                $this->grID = $grID;
-                $this->vidGroupElements = $vidGroupElements;
-            }
-
-    }
-
-
-        class vidBoxElement {
-        private $img ="";
-        private $ent_ID;
-        private $isInWatchlist;
-
-        public function getWatchListModule() {
-
-            // submit action to watchlist.php per php "get" when clicked
-            $str = "<a href='watchlist.php?action=status&ent_id=".$this->ent_ID."'>";
-
-            // if already watchlisted: ask to remove
-            if ($this->isInWatchlist == 1){
-                    $str .= "<h4 class='addwatchlist'>- Remove from Watchlist</h4>";
-            }
-            else{
-                    $str .= "<h4 class='addwatchlist'>+ Add to Watchlist</h4>";
-            }
-
-
-            $str .= "</a>";
-            return $str;
-
-        }
-
-        public function getString() {
-
-                    // print out image tag with correct source and basic styling
-
-            $str = "<div>";
-            $str .= "<a href=".getWatchURL($this->ent_ID).">";
-            $str .= "<img class= 'videobox' src=".$this->img.">";
-            $str .= "</a>";
-            $str .= $this->getWatchListModule();
-            $str .= "</div>";
-            return $str;
-        }
-        public function __construct($imgurl, $ent_id){
-
-            $this->img = $imgurl;
-            $this->ent_ID = $ent_id;
-
-            $this->isInWatchlist = checkIfWatchlisted($_SESSION['usr_id'],$this->ent_ID);
-
-
-
-
-        }
-
-    }
-
-    //TESTDATA!!!
-            /*
-                $test = array(
-                    new vidBoxElement("https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg",1),
-                    new vidBoxElement("https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg",1),
-                    new vidBoxElement("https://www.tasteofcinema.com/wp-content/uploads/2015/10/maxresdefault.jpg",1)
-                        );
-                $test2 = new vidBoxWrapper("Halloween Special", 1, $test);
-                echo $test2->getString();
-            */
-
-
-
+  // MOVED CLASSES FOR BOXES TO includes/classes/DisplayElements.php
 
     //get IDs for Groups
                             // first PARAMETER = MOVIE (0 or 1)
