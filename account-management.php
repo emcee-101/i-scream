b84a1f617b8fefb334310ff16ad354355eccc5db
VRtheonlyone <rodtvadim@gmail.com>
2022-01-15 16:50:11 +0100

    Applied HTML Generator to accoun-management.php

--- account-management.php
+++ account-management.php
@@ -30,28 +30,9 @@ var_dump($membership);
 $membership=$membership["isSubscribed"];
 
 
-
-
+$HTML = new SiteGenerator("Account","background2");
+$HTML->generateSiteStart();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="assets/css/style.css">
-    <script src="assets/css/js/script.js"></script>
-
-    <title>Account </title>
- 
-</head>
-
-<body class= "background2" >
-                
-                   
         <div class="box" id="cent">
         <h2 id="redfont">Account</h2>
         <form method="POST">
@@ -67,10 +48,6 @@ $membership=$membership["isSubscribed"];
         </div>
           
         </form>
-    
 
-
-</body>
-
-</html>
+<?$HTML->generateSiteStart();?>
 
