bb380a111d760068caa2d2d469e4639a944a2b0c
lu1943po <polina.luzina@fh-erfurt.de>
2022-01-15 09:59:24 +0100

    Update account-management.php

--- account-management.php
+++ account-management.php
@@ -1,6 +1,37 @@
 <?php
+session_start();
+include_once("includes/connection.php");
 require_once("includes/header.php");
 require_once("includes/footer.php");
+
+
+$con = establish_connection_db();
+$user_id = $_SESSION['usr_id'];
+
+
+$email = mysqli_query($con, "select mail_address from user where user_id='$user_id' ");
+$email=mysqli_fetch_assoc($email);
+var_dump($email);
+$email=$email["mail_address"];
+
+$password= mysqli_query($con, "select password from user where user_id='$user_id' ");
+$password=mysqli_fetch_assoc($password);
+var_dump($password);
+$password=$password["password"];
+
+$age= mysqli_query($con, "select age from user where user_id='$user_id' ");
+$age=mysqli_fetch_assoc($age);
+var_dump($age);
+$age=$age["age"];
+
+$membership= mysqli_query($con, "select isSubscribed from user where user_id='$user_id' ");
+$membership=mysqli_fetch_assoc($membership);
+var_dump($membership);
+$membership=$membership["isSubscribed"];
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -25,10 +56,10 @@ require_once("includes/footer.php");
         <h2 id="redfont">Account</h2>
         <form method="POST">
 
-        <p>Email<br> <input type="text" name="email"><br><br></p>
-        <p>Password<br> <input type="password" name="password"><br><br></p>
-        <p>Date of birth <br> <input type="date" name="Date of birth"><br><br>
-        <p>Membership<br><input type="text" name="membership"><br><br></p>
+        <p>Email:<br>  <p type="text" name="email"> <?php echo $email; ?>  </p>  <br></p> 
+        <p>Password:<br> <p type="password" name="password"><?php echo $password; ?>  </p> <br></p>
+        <p>Age: <br> <p type="date" name="Age"> <?php echo $age; ?> </p> <br> </p>
+        <p>Membership: <p type="text" name="membership"> <?php echo $membership; ?>  </p> <br><br></p>
 
             
         <p><input class="button button2" style="padding:10px 0px" value="Edit profile"><br>
@@ -42,3 +73,4 @@ require_once("includes/footer.php");
 </body>
 
 </html>
+
